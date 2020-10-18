457782716d26b677ae76813e702ff42a8a45769b
Bernardo Vargas <bernardo.vargasf@hotmail.com>
2020-10-18 17:39:26 -0500

    Menu Draft

--- src/main/java/com/jobsity/greenfield/App.java
+++ src/main/java/com/jobsity/greenfield/App.java
@@ -1,13 +1,35 @@
 package com.jobsity.greenfield;
 
-/**
- * Hello world!
- *
- */
+import java.util.Scanner;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	int opt = 0;
+    	while(opt != 3) {
+    		Scanner sc= new Scanner(System.in);
+    		System.out.println("Choose an option:");
+    		System.out.println("1. Read file");
+    		System.out.println("2. Show output");
+    		System.out.println("3. Exit");
+    		opt = sc.nextInt();
+    		switch(opt) {
+	    		case 1:
+	    			System.out.println("opcion 1");
+	    			// Read file and allocate to PlayerSet
+	    			break;
+	    		case 2:
+	    			System.out.println("opcion 2"); 
+	    			// Output of set, titles + player set string
+	    			break;
+	    		case 3:
+	    			System.out.println("Bye!"); break;
+	    		default:
+	    			System.out.println("Unknown choice");
+    		
+    		}
+    		sc.close();
+    	}
     }
 }
