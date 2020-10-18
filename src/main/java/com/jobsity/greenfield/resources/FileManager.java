e6d206e605a25f4318f4ac647521ba0de5debd9d
Bernardo Vargas <bernardo.vargasf@hotmail.com>
2020-10-18 17:56:19 -0500

    Create FileManager class

---
+++ src/main/java/com/jobsity/greenfield/resources/FileManager.java
@@ -0,0 +1,14 @@
+package com.jobsity.greenfield.resources;
+
+import java.io.BufferedWriter;
+
+public class FileManager {
+	
+	static void readFile(String fileName) {
+		// TODO: Add reading of the file
+	}
+	
+	static void writeFile(String fileName, BufferedWriter bw) {
+		// TODO: Write file
+	}
+}
