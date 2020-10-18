ce23be9bbd0aece2c2123f6af61c8b2b6763b394
Bernardo Vargas <bernardo.vargasf@hotmail.com>
2020-10-18 14:53:48 -0500

    Added Maven nature to project

---
+++ src/main/java/com/jobsity/greenfield/App.java
@@ -0,0 +1,13 @@
+package com.jobsity.greenfield;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
